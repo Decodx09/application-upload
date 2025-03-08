7836a97ebf42035e744c387ee5199a3150e18e51
shivansh <53109024+Decodx09@users.noreply.github.com>
2025-03-09 03:08:24 +0530

    Third Commit

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+require('dotenv').config();
+
 // Database connection
 const db = mysql.createConnection({
     host: 'your-rds-endpoint',
